6559b42ef3c39e4a5cd38b3c97cf930476d8905e
Expyron <tom.gonel@gmail.com>
2017-04-02 11:34:00 +0200

    Userscript

---
+++ userscript.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Green Lattice
+// @namespace    https://github.com/expyron/GreenLattice
+// @version      0.1
+// @author       Expyron
+// @include        https://www.reddit.com/place
+// @grant        none
+// ==/UserScript==
+
+var script = document.createElement('script');
+script.id = 'green_lattice';
+script.src = 'https://genbtc.github.io/AutoTrimps/AutoTrimps2.js';
+document.head.appendChild(script);
