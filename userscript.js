6d22a5bf762fce4790a8e62f7a6eb80813393153
Alberto Leal <mailforalberto@gmail.com>
2017-04-02 16:58:22 -0400

    cachebust — confirmed by some

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Green Lattice
 // @namespace    https://github.com/expyron/GreenLattice
-// @version      0.1
+// @version      0.2
 // @author       Expyron
 // @include      https://www.reddit.com/place*
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
@@ -14,6 +14,6 @@ window.setTimeout(function(){
   console.log("Loading Green Lattice");
   var script = document.createElement('script');
   script.id = 'green_lattice';
-  script.src = 'https://expyron.github.io/green_lattice/lattice.js';
+  script.src = 'https://expyron.github.io/green_lattice/lattice.js?t=' + (+ new Date());
   document.head.appendChild(script);
 }, 2 * 1000);
